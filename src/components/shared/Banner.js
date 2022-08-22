52c04dce3b686e6914e6dcaf7bc409ba05b5b79f
mohammadreza khodabandehlu <mamdrez.kh@gmail.com>
2022-08-22 11:34:28 +0430

    update redux

---
+++ src/components/shared/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Baneer from "../../assets/images/banner.png"
+
+import styles from "./Banner.module.css"
+
+const Banner = () => {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    );
+};
+
+export default Banner;
